05f46f4f6af28130d87240e3662e10d22fb99078
Oskar-Ciebien <oskarciebien@gmail.com>
2021-12-14 14:30:08 +0000

    Added a basic plan for Front-End on AboutUs

--- screens/AboutUs.js
+++ screens/AboutUs.js
@@ -3,23 +3,44 @@ import { View, Text, StyleSheet } from "react-native";
 
 export default function AboutUsScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>About Us Screen</Text>
+    // <View style={styles.heading}>
+
+    //   <Text>About Us</Text>
+    //   <View style={styles.container}>
+    //     <Text>Our Goal</Text>
+    //   </View>
+    // </View>
+    <View style={styles.heading}>
+      <View style={{ flex: 1, backgroundColor: "#256188" }}>
+        <Text
+          style={{
+            textAlign: "center",
+            //justifyContent: "center",
+            //alignItems: "center",
+            textAlignVertical: "center",
+            fontSize: 28,
+            paddingTop: 25,
+          }}
+        >
+          About Us
+        </Text>
+      </View>
+      <View style={{ flex: 4, backgroundColor: "white" }}>
+        <Text style={{ textAlign: "center", fontSize: 28, marginTop: 20 }}>
+          Our Goal:{" "}
+        </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  heading: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#256188",
+  },
   container: {
     flex: 1,
-    padding: 24,
-    backgroundColor: "#eaeaea",
-  },
-  heading: {
-    borderWidth: 4,
-    borderRadius: 6,
-    textAlign: "center",
-    fontSize: 30,
-    fontWeight: "bold",
   },
 });
