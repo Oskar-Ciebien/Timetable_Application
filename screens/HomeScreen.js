7b79af50d79d373c159d70eb0e21a329aff68529
Oskar-Ciebien <oskarciebien@gmail.com>
2022-02-24 15:55:34 +0000

    Removed Log Out Functionality from Home Page, and added style to the text, with SafeAreaView

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,39 +1,23 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 // Navigation
 import { useNavigation } from "@react-navigation/core";
 
 // Firebase
 import { auth } from "../firebase";
-import { signOut } from "firebase/auth";
 
 const HomeScreen = () => {
   // Navigation
   const navigation = useNavigation();
 
-  // Log Out
-  const logOut = () => {
-    signOut(auth)
-      .then(() => {
-        // Sign-out successful.
-        navigation.replace("Login");
-      })
-      .catch((error) => {
-        // An error happened.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log("Error: ", error.code, error.message);
-      });
-  };
   return (
-    <View style={styles.container}>
-      <Text>Your Email: {auth.currentUser?.email}</Text>
-
-      <TouchableOpacity onPress={logOut} style={styles.button}>
-        <Text style={styles.buttonText}>Log out</Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.text}>Your Email: {auth.currentUser?.email}</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -45,17 +29,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  button: {
-    alignItems: "center",
-    backgroundColor: "red",
-    width: "40%",
-    padding: 15,
-    borderRadius: 10,
-    marginTop: 60,
-  },
-  buttonText: {
-    color: "white",
-    fontWeight: "700",
-    fontSize: 16,
+  text: {
+    color: "black",
+    fontSize: 22,
   },
 });
